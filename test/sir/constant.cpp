3d7dd72bf29bcd89c4ae9cd6d95e28998ae08b02
Jessica Ray <jray@csail.mit.edu>
2021-03-22 20:43:33 -0400

    some stuff

--- test/sir/constant.cpp
+++ test/sir/constant.cpp
@@ -16,17 +16,6 @@ TEST_F(SIRCoreTest, ConstTypeQueryAndReplace) {
   ASSERT_EQ(1, node->replaceUsedType(module->getIntType(), module->getIntType()));
 }
 
-TEST_F(SIRCoreTest, ConstValueMatches) {
-  auto VALUE = 1;
-
-  auto *node = module->Nr<IntConst>(VALUE, module->getIntType());
-  ASSERT_EQ(VALUE, node->getVal());
-
-  std::stringstream s;
-  s << *node;
-  ASSERT_EQ(std::to_string(VALUE), s.str());
-}
-
 TEST_F(SIRCoreTest, ConstCloning) {
   auto VALUE = 1;
   auto *node = module->Nr<IntConst>(VALUE, module->getIntType());
@@ -35,14 +24,4 @@ TEST_F(SIRCoreTest, ConstCloning) {
   ASSERT_TRUE(clone);
   ASSERT_EQ(VALUE, clone->getVal());
   ASSERT_EQ(module->getIntType(), clone->getType());
-}
-
-TEST_F(SIRCoreTest, StringConstFormatting) {
-  auto VALUE = "hi";
-
-  auto *node = module->Nr<StringConst>(VALUE, module->getStringType());
-
-  std::stringstream s;
-  s << *node;
-  ASSERT_EQ(fmt::format(FMT_STRING("\"{}\""), VALUE), s.str());
 }
